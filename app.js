5483f6d40cea6ac5100e42663ad017efdecf7706
Vapaa Eero <eero.hallikainen@gmail.com>
2025-01-07 16:27:48 +0200

    fixes

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
 // get the port from env variable
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5001
 
-app.use(express.static("dist"));
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line
   console.log(`server started on port ${PORT}`);
-});
+})
